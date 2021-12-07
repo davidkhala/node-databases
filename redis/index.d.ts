6d666c6275f311d16e2d841b4d67819920aa0e21
DavidLiu <david.yx.liu@oracle.com>
2021-12-07 15:06:33 +0800

    fix redis

--- redis/index.d.ts
+++
@@ -1,6 +0,0 @@
-export default class Client {
-    private client;
-    constructor(endpoint: any, user: any, password: any);
-    connect(): Promise<void>;
-    disconnect(): Promise<void>;
-}
--- redis/index.js
+++ redis/index.js
@@ -1,12 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import { createClient } from 'redis';
 import KvDB from '@davidkhala/kvdb/index.js';
 export default class Client extends KvDB {
@@ -31,14 +22,10 @@ export default class Client extends KvDB {
     clear() {
         throw new Error('Method not implemented.');
     }
-    connect() {
-        return __awaiter(this, void 0, void 0, function* () {
-            yield this.client.connect();
-        });
+    async connect() {
+        await this.client.connect();
     }
-    disconnect() {
-        return __awaiter(this, void 0, void 0, function* () {
-            yield this.client.disconnect();
-        });
+    async disconnect() {
+        await this.client.disconnect();
     }
 }
