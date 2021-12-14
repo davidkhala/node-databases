747726c268e0543c4d77720d803089045d51cf5b
David Liu <david.yx.liu@oracle.com>
2021-12-14 09:25:40 +0800

    fix redis

--- redis/index.js
+++ redis/index.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import { createClient } from 'redis';
 import KvDB from '@davidkhala/kvdb/index.js';
 export default class Client extends KvDB {
@@ -22,10 +31,14 @@ export default class Client extends KvDB {
     clear() {
         throw new Error('Method not implemented.');
     }
-    async connect() {
-        await this.client.connect();
+    connect() {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield this.client.connect();
+        });
     }
-    async disconnect() {
-        await this.client.disconnect();
+    disconnect() {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield this.client.disconnect();
+        });
     }
 }
