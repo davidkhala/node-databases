b8d27113f445d3e56a75bde00141cddb0c19af11
David Liu <david.yx.liu@oracle.com>
2022-11-21 22:02:51 +0800

    [done] teradata vbox connect

--- teradata/index.js
+++ teradata/index.js
@@ -4,27 +4,50 @@ export default class Teradata {
 
     constructor({host, username = 'dbc', password = 'dbc'}) {
         this.client = new TeradataConnection();
-        this.connect({
-            host,
-            user: username,
-            password,
-        })
+        Object.assign(this, {host, username, password})
     }
 
-    connect(opts) {
-        this.client.connect(opts)
+    connect() {
+        const {username: user, host, password} = this
+        this.client.connect({host, user, password})
         this.cursor = this.client.cursor();
     }
 
-    version() {
-
-        this.cursor.execute('{fn teradata_nativesql}Database version{fn teradata_database_version}');
-        const rows = this.cursor.fetchone();
-        return rows[0]
+    execute(sql) {
+        this.cursor.execute(sql);
     }
+
+    query(sql) {
+        this.execute(sql)
+        return this.cursor.fetchall()
+    }
+
 
     disconnect() {
         this.cursor.close();
         this.client.close();
+    }
+}
+
+export class SystemInfo extends Teradata {
+    session() {
+        this.execute('help session');
+        const row = this.cursor.fetchone()
+
+        let result = {}
+        row.forEach((field, index) => {
+            result[this.cursor.description[index][0]] = field
+        });
+        return result
+    }
+
+    version() {
+        this.execute('{fn teradata_nativesql}Database version{fn teradata_database_version}');
+        const rows = this.cursor.fetchone();
+        return rows[0]
+    }
+
+    static buildFrom(teradata) {
+        return new SystemInfo(teradata)
     }
 }
--- teradata/test/index.js
+++ teradata/test/index.js
@@ -1,11 +1,15 @@
-import Teradata from '../index.js'
+import Teradata, {SystemInfo} from '../index.js'
 
 describe('Connect', function () {
     this.timeout(0)
-    it('in vbox', async () => {
-        const teradata = new Teradata({host: '192.168.3.155'})
-        teradata.version()
 
-        teradata.disconnect()
+    it('get info', async () => {
+        const systemInfo = new SystemInfo({host: '192.168.3.155'})
+        systemInfo.connect()
+        systemInfo.session()
+        systemInfo.disconnect()
+    })
+    after(() => {
+
     })
 })
