d20611ff17a362cf9d53a02c0b135616cfdd2367
David Liu <david-khala@hotmail.com>
2022-11-24 22:28:57 +0800

    code clean

--- teradata/test/index.js
+++ teradata/test/index.js
@@ -1,15 +1,13 @@
-import Teradata, {SystemInfo} from '../index.js'
+import {SystemInfo} from '../index.js'
 
 describe('Connect', function () {
     this.timeout(0)
 
     it('get info', async () => {
-        const systemInfo = new SystemInfo({host: '192.168.3.155'})
+        const systemInfo = new SystemInfo({host: 'localhost'})
         systemInfo.connect()
         systemInfo.session()
         systemInfo.disconnect()
     })
-    after(() => {
 
-    })
 })
