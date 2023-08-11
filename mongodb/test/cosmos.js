f83e07c5b0ae9137e4103b9dc60840275ab98133
David Liu <david-khala@hotmail.com>
2023-08-11 14:00:35 +0800

    [WIP] gremlin server connection

--- mongodb/test/cosmos.js
+++ mongodb/test/cosmos.js
@@ -1,20 +1,15 @@
 import Cosmos from '../cosmos.js'
 
 describe('cosmos', function () {
-	this.timeout(0)
-	const {cosmos_password: password} = process.env
+    this.timeout(0)
+    const {cosmos_password: password} = process.env
 
-	it('connectString', async () => {
-		const connectString = `mongodb://converged-cosmos:${password}@converged-cosmos.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@converged-cosmos@`
-		const connect = new Cosmos({}, connectString)
-		await connect.connect()
-		await connect.disconnect()
-	})
-	it('dbName & token', async () => {
-		const dbName = 'converged-cosmos'
-		const connect = new Cosmos({dbName, token: password})
-		await connect.connect()
-		await connect.disconnect()
-	})
+    const dbName = 'mongo-davidkhala'
+    it('dbName & token', async () => {
+
+        const connect = new Cosmos({dbName, token: password})
+        await connect.connect()
+        await connect.disconnect()
+    })
 
 })
