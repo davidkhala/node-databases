4e25ed8f9f97f7a797ac9ca32c626b4b2d9c7dbd
David Liu <david-khala@hotmail.com>
2023-08-26 17:09:35 +0800

    @davidkhala/tinkerpop: 0.0.3-alpha.1 handle compatibility

--- tinkerpop/gremlin-server.js
+++ tinkerpop/gremlin-server.js
@@ -1,9 +1,19 @@
 import {AbstractGremlin} from "./index.js";
+import assert from "assert";
 
 export class GremlinServer extends AbstractGremlin {
     constructor({domain = 'localhost', port = 8182} = {}, logger = console.debug) {
         super({domain, port, name: 'gremlin', dialect: 'ws'}, undefined, logger);
 
+    }
+
+    /**
+     * @param {GraphTraversal} traversal
+     */
+    async createV(traversal) {
+        const results = await GremlinServer.query(traversal)
+        assert.ok(results.length < 2)
+        return results[0].id
     }
 
 }
