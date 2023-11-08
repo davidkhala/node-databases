aea6f4fa2bc3e33a0601ea72f4517a8660eaa6b9
David Liu <david-khala@hotmail.com>
2023-11-08 20:29:33 +0800

    fix AuraDB CI

--- neo4j/test-aura/aura.js
+++ neo4j/test-aura/aura.js
@@ -1,21 +1,22 @@
 import {AuraDB} from '../aura.js';
 import {Neo4jAdmin} from '../index.js';
-import {querySet} from './recipe.js';
+import {querySet} from '../test/recipe.js';
 
 describe('AuraDB', function () {
 	this.timeout(0);
 	const password = process.env.NEO4J_PASSWORD;
-	const neo4j = new AuraDB({instance: '9e9db278', password});
+	const neo4j = new AuraDB({instance: '0d0be935', password});
 	const dba = new Neo4jAdmin(neo4j);
 	before(async () => {
 		await neo4j.connect();
 		await dba.clear();
 	});
+	it('connect', () => {
+	});
 	it('query', async () => {
 		await querySet(neo4j);
 	});
-	it('connect', () => {
-	});
+
 	after(async () => {
 		await neo4j.disconnect();
 	});
