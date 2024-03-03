4ca037fc89c08500dba7f7cd9e5aa4415c14f1c7
David Liu <david-khala@hotmail.com>
2024-03-03 12:41:30 +0800

    postgresql: 0.0.1

--- postgre/index.js
+++ postgre/index.js
@@ -1,24 +1,24 @@
 import pg from 'pg';
 import DB from '@davidkhala/db/index.js';
-import assert from 'assert';
 
 
 const {Client} = pg;
 
 export default class PostGRE extends DB {
 	/**
-     *
-     * @param domain
-     * @param [port]
-     * @param [connectionString]
-     * @param [query_timeout]
-     * @param [username]
-     * @param [password]
-     */
+	 *
+	 * @param domain
+	 * @param [port]
+	 * @param [connectionString]
+	 * @param [query_timeout]
+	 * @param [username]
+	 * @param [password]
+	 */
 	constructor({
 		domain, port = 5432, username = 'postgres', password, name
 	}, connectionString) {
-		super({domain, username, password, port, name, dialect: 'postgresql'}, connectionString);
+		const dialect = 'postgres'; // both 'postgresql' or 'postgres' are OK
+		super({domain, username, password, port, name, dialect}, connectionString);
 
 		this.connection = new Client({
 			user: username, port, host: domain, password, database: name,
@@ -34,9 +34,9 @@ export default class PostGRE extends DB {
 	}
 
 	async query(sqlTemplate, values) {
-		const result = await this.client.query(sqlTemplate, values);
+		const result = await this.connection.query(sqlTemplate, values);
 		const {rowCount, rows, fields} = result;
-		return {rows, fields};
+		return {rows, fields, rowCount};
 	}
 }
 
